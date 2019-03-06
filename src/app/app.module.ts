dd70f42e87167fb2816d1f87ee2f4fafd94f74a6
Michal Kaczmarek <michal.kaczmarek@exaco.pl>
2019-03-06 14:39:04 +0100

    page page item components and refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { PageHomeComponent } from './page-home/page-home.component';
-import { JumbotronDirective } from './directives/jumbotron.directive';
-import { HideMeDirective } from './directives/hide-me.directive';
+import { AppComponent } from './components/app/app.component';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { JumbotronDirective } from './directives/jumbotron/jumbotron.directive';
+import { HideMeDirective } from './directives/hide-me/hide-me.directive';
+import { PageMovieListComponent } from './pages/page-movie-list/page-movie-list.component';
+import { PageContactComponent } from './pages/page-contact/page-contact.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { MovieListItemComponent } from './components/movie-list-item/movie-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageHomeComponent,
     JumbotronDirective,
-    HideMeDirective
+    HideMeDirective,
+    PageMovieListComponent,
+    PageContactComponent,
+    MovieListComponent,
+    MovieListItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/page-movie-list/page-movie-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-movie-list',
+  templateUrl: './page-movie-list.component.html',
+  styleUrls: ['./page-movie-list.component.css']
+})
+export class PageMovieListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
