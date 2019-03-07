362f30e18edd5a180a71592845a5c46fe61c686a
Michal Kaczmarek <michal.kaczmarek@exaco.pl>
2019-03-07 10:38:44 +0100

    Remove changes rate on list item

--- src/app/components/movie-list-item/movie-list-item.component.ts
+++ src/app/components/movie-list-item/movie-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Movie } from 'src/app/interfaces/movie.interface';
@@ -15,15 +15,10 @@ export class MovieListItemComponent implements OnInit {
   @Input() movie: Movie = null;
 
   constructor(
-    private router: Router,
-    private changeDetector: ChangeDetectorRef
+    private router: Router
   ) { }
 
   ngOnInit() {
-    setTimeout(()=>{
-      this.movie.rate = 10;
-      this.changeDetector.markForCheck() // Informujemy o zmianach wewnątrz komponentu, domyślnie zmiany są wykrywalne tylko z zewnątrz
-    }, 2000)
   }
 
   redirectMovieProfile() {
