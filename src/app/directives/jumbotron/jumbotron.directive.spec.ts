4d9d03b6bc65cae68732ca0a507a6377678110ae
Michal Kaczmarek <michal.kaczmarek@exaco.pl>
2019-03-07 08:21:05 +0100

    Test for directive with mocks

--- src/app/directives/jumbotron/jumbotron.directive.spec.ts
+++ src/app/directives/jumbotron/jumbotron.directive.spec.ts
@@ -1,8 +1,25 @@
 import { JumbotronDirective } from './jumbotron.directive';
+import { ElementRef } from '@angular/core';
 
 describe('JumbotronDirective', () => {
+
   it('should create an instance', () => {
     const directive = new JumbotronDirective(null);
     expect(directive).toBeTruthy();
   });
+
+  it('css class have been added', () => {
+    const elementify = {
+      nativeElement: {
+        classList: {
+          add: () => { }
+        }
+      }
+    }
+    spyOn(elementify.nativeElement.classList, 'add')  // szpieg nasłuchujący wywołania metody
+    const directive = new JumbotronDirective(<ElementRef>elementify);
+    directive.ngOnInit();
+    expect(elementify.nativeElement.classList.add).toHaveBeenCalled()
+  });
+
 });
