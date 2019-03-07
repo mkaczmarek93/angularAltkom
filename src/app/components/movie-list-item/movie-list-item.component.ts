5b11d8b8f04edc482d2e569aa3ec0225d97998b2
Michal Kaczmarek <michal.kaczmarek@exaco.pl>
2019-03-07 10:17:24 +0100

    detect changes on item and on list

--- src/app/components/movie-list-item/movie-list-item.component.ts
+++ src/app/components/movie-list-item/movie-list-item.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit, Input, ViewEncapsulation, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Movie } from 'src/app/interfaces/movie.interface';
 
@@ -7,21 +7,32 @@ import { Movie } from 'src/app/interfaces/movie.interface';
   selector: 'app-movie-list-item',
   templateUrl: './movie-list-item.component.html',
   styleUrls: ['./movie-list-item.component.css'],
-  encapsulation: ViewEncapsulation.Native
+  encapsulation: ViewEncapsulation.Native,
+  changeDetection: ChangeDetectionStrategy.OnPush // Odświeżenie komponentu tylko kiedy są w nim jakieś zminny
 })
 export class MovieListItemComponent implements OnInit {
 
   @Input() movie: Movie = null;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private changeDetector: ChangeDetectorRef
   ) { }
 
   ngOnInit() {
+    setTimeout(()=>{
+      this.movie.rate = 10;
+      this.changeDetector.markForCheck() // Informujemy o zmianach wewnątrz komponentu, domyślnie zmiany są wykrywalne tylko z zewnątrz
+    }, 2000)
   }
 
   redirectMovieProfile() {
     this.router.navigateByUrl('movies/' + this.movie.id);
+  }
+
+  buildRate() {
+    console.log(1)
+    return this.movie.rate * 10 + '%'
   }
 
 }
--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
+  styleUrls: ['./movie-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MovieListComponent implements OnInit {
 
